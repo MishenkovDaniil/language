2119a666effe3f8b4237bc24452596220d1bccdc
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-12-18 02:23:44 +0300

    placed operation types in separatory header

---
+++ frontend/op_types.h
@@ -0,0 +1,18 @@
+#ifndef OP_TYPES
+#define OP_TYPES
+
+
+enum Op_types
+{
+    ADD = 1,
+    SUB = 2,
+    MUL = 3,
+    DIV = 4,
+    DEG = 5,
+
+    AND = 7,
+    OR  = 8,
+    NOT = 9,
+};
+
+#endif /* OP_TYPES */
